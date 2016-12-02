7ba672f19550beeb448660f5e888c40ca9f3fc79
samuel.urbanowicz <samiuelson@gmail.com>
2016-12-02 23:28:01 +0100

    Implement Tag class

--- data/Models.py
+++ data/Models.py
@@ -2,7 +2,8 @@ from app import db
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    fbId = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
 
     def __init__(self, id, name):
@@ -11,3 +12,18 @@ class User(db.Model):
 
     def __repr__(self):
         return "User id %r" % self.id
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    key = db.Column(db.String, nullable=False)
+    enabled = db.Column(db.Boolean, default=True)
+
+    def __init__(self, key, enabled):
+        self.key = key
+        self.enabled = enabled
+
+    def __repr__(self):
+        return "Tag %r" % self.key
+
+class SentTag(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)

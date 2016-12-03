d99be5301601435f20d65cde71f174df581e4d5d
samuel.urbanowicz <samiuelson@gmail.com>
2016-12-03 20:21:06 +0100

    Refactor app instance creation. Create script for db creation

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
-db = SQLAlchemy()
-
-
-@app.route('/')
 def hello_world():
     return "Because I'm happy!"
 
+
+def init_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
+    app.add_url_rule('/', view_func=hello_world)
+    app.app_context().push()
+    return app
+
+app = init_app()
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run()

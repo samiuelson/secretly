8a6401ffd1c4014ce7faed7afdf7ccb45eb36c3e
samuel.urbanowicz <samiuelson@gmail.com>
2016-12-03 20:20:10 +0100

    Extract configuration to external file

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI =  'sqlite:///' + os.path.join(basedir, 's3cretly.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_migrate_repository')
+BASIC_AUTH_USERNAME = 'login'
+BASIC_AUTH_PASSWORD = 'pass'

52969d15225a56f6cc0dbb8c31f330f67b0c047d
samuel.urbanowicz <samiuelson@gmail.com>
2016-12-11 14:10:47 +0100

    Enable travis build

---
+++ rest/UserAuthorization.py
@@ -0,0 +1,15 @@
+from flask_restful import Resource
+
+class UserAuthorization(Resource):
+
+    ''' Sends user data in post request in order to register it '''
+    def post(self, **kwargs):
+
+
+        return
+
+    ''' Checks if user is currentrly registered to Secretly backend '''
+    def get(self, **kwargs):
+
+
+        return
